5c03a3a9edc2cb33f3a9dbb271a832720691cfe0
Roshan <275509523@qq.com>
2018-01-09 10:32:23 +0800

    修改lconfig  配置

--- admin/lconfig.php
+++ admin/lconfig.php
@@ -27,8 +27,8 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'Iamjxshh0928!');
+define('DB_PASSWORD', 'iammysqlroot');
+define('DB_DATABASE', 'myopencart');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
