0144910dd4295a7efdfc7982063375b642a2be92
MengFanGang <1417502518@qq.com>
2018-04-19 16:38:39 +0800

    像素

--- catalog/controller/product/myproduct.php
+++ catalog/controller/product/myproduct.php
@@ -97,7 +97,7 @@ class ControllerProductMyproduct extends Controller {
         $data['http'] = HTTP_SERVER;
         $data['price'] = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->myCurrency);
         $data['origin_price'] = $this->currency->format($this->tax->calculate($product_info['price'] + 100, $product_info['tax_class_id'], $this->config->get('config_tax')), $this->myCurrency);
-        $data['price_num'] = $product_info['price'];
+        $data['price_num'] =sprintf("%.2f",$product_info['price']) ;
         $data['title'] = $product_info['meta_title'];
         $data['name'] = $product_info['name'];
 //        $data['product_id'] = (int)$this->request->get['product_id'];
